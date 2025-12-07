724fe834c505aa4d8b6c80790335014e753c5bbd
Tomasz Zygadlo <t.zyadlo@leadya.ai>
2025-12-07 09:05:10 +0100

    Z inputem

--- Zadanie testowe.py
+++ Zadanie testowe.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello, World!")
+    name = input("Podaj imię: ").strip()
+    if not name:
+        name = "World"
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     main()
